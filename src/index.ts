682cc9d0588d2d2f46618a20be89bfe0bfd1e7b3
Kunal Khare <136145336+kunaldevxxx@users.noreply.github.com>
2024-01-01 01:20:37 +0530

    Code Refactor

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,21 @@
 import express from 'express'
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
-import { prismaclient } from './lib/db';
+import createGraphqlServer from './graphql';
+// import { prismaclient } from './lib/db';
 
 async function init() {
-    const app = express()
+const app = express()
 const PORT = Number(process.env.PORT)|| 8000
-app.use(express.json())
-//Create GraphQL Server
-const server = new ApolloServer({
-    typeDefs:`
-    type Query {
-        hello:String,
-        say(name:String):String,
-    }
-    type Mutation {
-        createUser(firstName:String!,lastName:String!,email:String!,password:String!): Boolean
-    } 
-    `, //schemas
-    resolvers:{
-        Query: {
-            hello:()=>`Hey There I Am Server`,
-            say:(_,{name}:{name:string})=>`Hey ${name}`,
-        },
-        Mutation: {
-            createUser:async(_,{firstName,lastName,email,password}:{firstName:string;lastName:string;email:string;password:string})=>{
-                await  prismaclient.user.create({
-                    data:{
-                        firstName,
-                        lastName,
-                        email,
-                        password,
-                        salt:"random_salt",
-                    },
-                });
-                return true;
-            }
-        }
-        
-        
-    },//resolvers or actual function
-  });
-//Start GraphQL Server
-await server.start();
+app.use(express.json());
+
 app.get('/',(req,res)=>{
     res.json({message:'Server Is Up And Running'})
-})
-app.use('/graphql', expressMiddleware(server));
+});
 
-app.listen(PORT,()=> console.log(`Server Started At PORT:${PORT}`))
+app.use('/graphql', expressMiddleware(await createGraphqlServer()));
+
+app.listen(PORT,()=> console.log(`Server Started At PORT:${PORT}`));
     
 }
 init();
